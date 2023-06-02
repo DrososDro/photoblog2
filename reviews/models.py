d859e887569de7dc5fa22f358a76b5d9e5dcdbfd
Drosinakis Drosos <drosinakis.drosos1@gmail.com>
2023-06-02 20:06:39 +0300

    fix bug with images and in reviews i add the name of the reviewer

--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 import uuid
 from django.core.validators import MaxValueValidator
+from account.models import Account
 
 # Create your models here.
 
 
 class Review(models.Model):
+    owner = models.ForeignKey(Account, models.CASCADE)
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
     star_rating = models.FloatField(
@@ -27,3 +29,6 @@ class Review(models.Model):
         if float(self.star_rating) < 0 or float(self.star_rating) > 5:
             self.star_rating = 0
         return super(Review, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
