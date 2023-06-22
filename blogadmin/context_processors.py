b8c92e4629202d1e9875dd502f6ddc871dceed8a
Drosos Drosinakis <drosinakis.drosos1@gmail.com>
2023-06-22 03:38:30 +0300

    finish with the project

--- blogadmin/context_processors.py
+++ blogadmin/context_processors.py
@@ -2,5 +2,8 @@ from .models import BlogInfo
 
 
 def blog_info(request):
-    blog = BlogInfo.objects.all()[0]
-    return dict(blog_info=blog)
+    try:
+        blog = BlogInfo.objects.all()[0]
+        return dict(blog_info=blog)
+    except:
+        return []
